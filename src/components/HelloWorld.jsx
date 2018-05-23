01452304486081317eb4d7b54acfb6611ebb4290
Steve <spdevuk@gmail.com>
2018-05-23 01:30:24 +0100

    hey!

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -8,6 +8,7 @@ class HelloWorld extends Component {
         <p> Hello World </p>
         <Link to="/" className="button">
           <button>Back to landing</button>
+          hey!
         </Link>
       </div>
     );

01c1cd23e894f5149f0884d78632a8807830ee89
Steve <spdevuk@gmail.com>
2018-05-30 13:13:09 +0100

    Added material colors & set up theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import ReactAux from './Components/hoc/ReactAux';
+import Navbar from './Components/Layout/Navbar/Navbar';
 import './styles/app.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#F3F3F3',
+      main: '#FAFAFA',
+      dark: '#B4B4B4',
+      contrastText: '#212121'
+    },
+    secondary: {
+      light: '#3F51B5',
+      main: '#3F51B5',
+      dark: '#192780',
+      contrastText: '#F3F3F3'
+    }
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <ReactAux>
+      <MuiThemeProvider theme={theme}>
         <CssBaseline />
-        <p> hi</p>
-      </ReactAux>
+        <Navbar />
+      </MuiThemeProvider>
     );
   }
 }

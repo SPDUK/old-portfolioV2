95bad88f65750c0a96a8a035ecc56d0e758c46a0
Steve <spdevuk@gmail.com>
2018-05-31 06:54:55 +0100

    added container class & cleaned up navbar a little

--- src/Components/Layout/Navbar/Navbar.jsx
+++ src/Components/Layout/Navbar/Navbar.jsx
@@ -19,24 +19,24 @@ class Navbar extends Component {
 
   render() {
     return (
-      <div className="nav">
+      <div className="nav container">
         <div className="nav-icon">
           <a href="/">SP</a>
         </div>
         <div className="nav-menu">
-          <Button>Projects</Button>
-          <Button>Extras</Button>
-          <Button>
+          <Button className="nav-menu-item">Projects</Button>
+          <Button className="nav-menu-item">Extras</Button>
+          <Button className="nav-menu-item">
             <strong>Contact</strong>
           </Button>
+          <Drawer right={this.state.right} toggleDrawer={this.toggleDrawer} />
+          <IconButton
+            onClick={this.toggleDrawer('right', true)}
+            className="nav-menu-burger"
+          >
+            <MenuIcon />
+          </IconButton>
         </div>
-        <Drawer right={this.state.right} toggleDrawer={this.toggleDrawer} />
-        <IconButton
-          onClick={this.toggleDrawer('right', true)}
-          className="nav-burger"
-        >
-          <MenuIcon />
-        </IconButton>
       </div>
     );
   }

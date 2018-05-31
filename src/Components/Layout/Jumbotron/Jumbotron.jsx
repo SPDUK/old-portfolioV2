af2252066ce8ae40563166cb4574aa36eae35fa9
Steve <spdevuk@gmail.com>
2018-05-31 08:07:51 +0100

    Made navbar text bigger + Added responsive h1 text for jumbo

--- src/Components/Layout/Jumbotron/Jumbotron.jsx
+++ src/Components/Layout/Jumbotron/Jumbotron.jsx
@@ -7,7 +7,13 @@ class Jumbotron extends Component {
     return (
       <div className="jumbo">
         <div className="container">
-          <span>hi</span>
+          <div className="jumbo-text">
+            <h1>
+              I'm Steve Parker, a JavaScript{' '}
+              <a href="https://github.com/SPDUK">Developer</a> Currently living
+              in Peterborough, England
+            </h1>
+          </div>
         </div>
       </div>
     );

8417ce04cccc93d1e24148b8915972c1d8816af8
SPDUK <33531423+SPDUK@users.noreply.github.com>
2018-06-26 17:32:45 +0100

    added chart for github & changed colors

--- src/Components/Layout/Github/Github.jsx
+++ src/Components/Layout/Github/Github.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-
 import './Github.css';
+import ChartistGraph from 'react-chartist';
 
 export default class Github extends Component {
   render() {
+    const lineChartData = {
+      labels: [0, 2, 3, 4, 5, 6],
+      series: [[0, 1, 2, 3, 4, 5]]
+    };
+    const lineChartOptions = {
+      low: 0,
+      showArea: true,
+      fullWidth: true
+    };
+
     return (
       <div className="container github">
         <h1>Github</h1>
+        <ChartistGraph
+          className="ct-octave"
+          data={lineChartData}
+          options={lineChartOptions}
+          type="Line"
+        />
       </div>
     );
   }

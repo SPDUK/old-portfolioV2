56c70d3fcddaf2398e8a474d82c60903429a1bf2
SPDUK <33531423+SPDUK@users.noreply.github.com>
2018-06-26 16:50:06 +0100

    added and set up start of  github component

---
+++ src/Components/Layout/Github/Github.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './Github.css';
+
+export default class Github extends Component {
+  render() {
+    return (
+      <div className="container github">
+        <h1>Github</h1>
+      </div>
+    );
+  }
+}

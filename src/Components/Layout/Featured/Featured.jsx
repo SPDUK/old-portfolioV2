b2f98c393e83ecd929421a841e1d5f1b4d5a2f92
SPDUK <33531423+SPDUK@users.noreply.github.com>
2018-08-01 02:58:07 +0100

    removed images & added descriptions and lazyloading

--- src/Components/Layout/Featured/Featured.jsx
+++ src/Components/Layout/Featured/Featured.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
+import LazyLoad from 'react-lazyload';
 
 import FeaturedProject from './FeaturedProject/FeaturedProject';
 
@@ -17,36 +18,38 @@ function Featured(props) {
     <div className="featured">
       <div className="container">
         <h1>Featured Personal Projects</h1>
-        <Grid className={classes.root} container spacing={24}>
-          <Grid item md={6} xs={12}>
-            <FeaturedProject
-              image="https://res.cloudinary.com/dmjolhdaq/image/upload/v1527838905/dgdmac.jpg"
-              title="Dance Gavin Dance"
-              link="https://spduk.github.io/dgd/"
-            />
+        <LazyLoad height={500}>
+          <Grid className={classes.root} container spacing={24}>
+            <Grid item md={6} xs={12}>
+              <FeaturedProject
+                image="https://res.cloudinary.com/dmjolhdaq/image/upload/v1527838905/dgdmac.jpg"
+                title="Dance Gavin Dance"
+                link="https://spduk.github.io/dgd/"
+              />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <FeaturedProject
+                image="https://res.cloudinary.com/dmjolhdaq/image/upload/v1529670340/Portfolio/fira-minified.jpg"
+                title="Startpage"
+                link="https://startpage.fira.app"
+              />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <FeaturedProject
+                image="https://res.cloudinary.com/dmjolhdaq/image/upload/v1532426260/Portfolio/quickprojectsmac.jpg"
+                title="Quick Projects"
+                link="https://spduk.github.io/Quick-Projects/#/"
+              />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <FeaturedProject
+                image="https://res.cloudinary.com/dmjolhdaq/image/upload/v1532426260/Portfolio/tradingpostmac.jpg"
+                title="Trading Post"
+                link="https://spduk.github.io/tradingpost/#/"
+              />
+            </Grid>
           </Grid>
-          <Grid item md={6} xs={12}>
-            <FeaturedProject
-              image="https://res.cloudinary.com/dmjolhdaq/image/upload/v1529670340/Portfolio/fira-minified.jpg"
-              title="Startpage"
-              link="https://startpage.fira.app"
-            />
-          </Grid>
-          <Grid item md={6} xs={12}>
-            <FeaturedProject
-              image="https://res.cloudinary.com/dmjolhdaq/image/upload/v1532426260/Portfolio/quickprojectsmac.jpg"
-              title="Quick Projects"
-              link="https://spduk.github.io/Quick-Projects/#/"
-            />
-          </Grid>
-          <Grid item md={6} xs={12}>
-            <FeaturedProject
-              image="https://res.cloudinary.com/dmjolhdaq/image/upload/v1532426260/Portfolio/tradingpostmac.jpg"
-              title="Trading Post"
-              link="https://spduk.github.io/tradingpost/#/"
-            />
-          </Grid>
-        </Grid>
+        </LazyLoad>
       </div>
     </div>
   );

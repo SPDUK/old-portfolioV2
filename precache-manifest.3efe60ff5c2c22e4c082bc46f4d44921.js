5271dd96c33814dc4f7c6f88dcf0b4973b8dbadd
SPDUK <spdevuk@gmail.com>
2018-11-01 20:32:11 +0000

    Updates

---
+++ precache-manifest.3efe60ff5c2c22e4c082bc46f4d44921.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c0a72fff29b5b8c4582",
+    "url": "/static/js/main.3c0a72ff.chunk.js"
+  },
+  {
+    "revision": "2a7f568850ecf28a6a2c",
+    "url": "/static/js/1.2a7f5688.chunk.js"
+  },
+  {
+    "revision": "3c0a72fff29b5b8c4582",
+    "url": "/static/css/main.91cf3c02.chunk.css"
+  },
+  {
+    "revision": "d37bf374ae1aa5b7828efbf26a602d01",
+    "url": "/index.html"
+  }
+];

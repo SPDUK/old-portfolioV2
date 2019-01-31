cd8cb59e5e57a555745ff5ce9738c81afe24388a
SPDUK <spdevuk@gmail.com>
2019-01-31 15:13:32 +0000

    Updating graph and using yarn instead of npm

--- src/Components/Layout/Github/Github.jsx
+++ src/Components/Layout/Github/Github.jsx
@@ -4,8 +4,8 @@ import ChartistGraph from 'react-chartist';
 export default class Github extends Component {
   render() {
     const lineChartData = {
-      labels: ['Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov'],
-      series: [[264, 338, 291, 88, 174, 71]]
+      labels: ['Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan'],
+      series: [[291, 88, 174, 71, 104, 112]]
     };
     const lineChartOptions = {
       low: 0,
@@ -17,7 +17,12 @@ export default class Github extends Component {
     return (
       <div className="container github">
         <h1>Git Commits in the last 6 months</h1>
-        <ChartistGraph className="ct-major-twelfth" data={lineChartData} options={lineChartOptions} type="Line" />
+        <ChartistGraph
+          className="ct-major-twelfth"
+          data={lineChartData}
+          options={lineChartOptions}
+          type="Line"
+        />
       </div>
     );
   }
